da93c735dca8ea87d1f10d1b9d04d17890d1a0fa
pocketberserker <pocketberserker@gmail.com>
2015-01-03 19:39:43 +0900

    add instances

--- src/Persimmon.Dried/Arbitrary.fs
+++ src/Persimmon.Dried/Arbitrary.fs
@@ -15,3 +15,45 @@ with
     Gen = this.Gen
     PrettyPrinter = this.PrettyPrinter
   }
+
+[<RequireQualifiedAccess>]
+module Arb =
+
+  let unit = {
+    Gen = Gen.constant ()
+    Shrinker = Shrink.shrinkAny
+    PrettyPrinter = fun () -> Pretty(fun _ -> "unit")
+  }
+
+  let bool = {
+    Gen = Gen.elements [ true; false ]
+    Shrinker = Shrink.shrinkAny
+    PrettyPrinter = Pretty.prettyAny
+  }
+
+  open FsRandom
+  open System
+
+  let byte = {
+    Gen = Gen.choose (Statistics.uniformDiscrete (int Byte.MinValue, int Byte.MaxValue)) |> Gen.map byte
+    Shrinker = Shrink.shrinkByte
+    PrettyPrinter = Pretty.prettyAny
+  }
+
+  let sbyte = {
+    Gen = Gen.choose (Statistics.uniformDiscrete (int SByte.MinValue, int SByte.MaxValue)) |> Gen.map sbyte
+    Shrinker = Shrink.shrinkSbyte
+    PrettyPrinter = Pretty.prettyAny
+  }
+
+  let int16 = {
+    Gen = Gen.choose (Statistics.uniformDiscrete (int Int16.MinValue, int Int16.MaxValue)) |> Gen.map int16
+    Shrinker = Shrink.shrinkInt16
+    PrettyPrinter = Pretty.prettyAny
+  }
+
+  let int = {
+    Gen = Gen.choose (Statistics.uniformDiscrete (Int32.MinValue, Int32.MaxValue))
+    Shrinker = Shrink.shrinkInt
+    PrettyPrinter = Pretty.prettyAny
+  }
--- src/Persimmon.Dried/Shrink.fs
+++ src/Persimmon.Dried/Shrink.fs
@@ -47,10 +47,79 @@ module Shrink =
         }
       }
 
-  let rec private shrinkOne s zs =
+  let rec private shrinkOne (s: Shrink<_>) zs =
     if Seq.isEmpty zs then Seq.empty
     else
       let x = Seq.head zs
       let xs = Seq.skip 1 zs
-      let a = shrink x s |> Seq.map (fun x -> seq { yield x; yield! xs })
+      let a = shrink s x |> Seq.map (fun x -> seq { yield x; yield! xs })
       Seq.append a (shrinkOne s xs |> Seq.map (fun xs -> seq { yield x; yield! xs }))
+
+  let private shrinkContainer (s: Shrink<'T>) v = apply (fun xs ->
+    Seq.append (removeChunks (Seq.length xs) xs) (shrinkOne s xs)
+    |> Seq.map v)
+
+  let shrinkSeq s = shrinkContainer s id
+  let shrinkList s = shrinkContainer s Seq.toList
+  let shrinkArray s = shrinkContainer s Seq.toArray
+
+  // FIXME
+  let private shrinkUInt' f g n =
+
+    let n = f n
+
+    let rec halfs n =
+      if n = 0UL then Seq.empty else seq { yield n; yield! halfs (n / 2UL) }
+
+    if n = 0UL then Seq.empty
+    else
+      let ns = halfs (n / 2UL) |> Seq.map (fun x -> n - x)
+      seq { yield 0UL; yield! interleave ns ns }
+    |> Seq.map g
+
+  let private shrinkInt' f g n =
+    
+    let n = f n
+
+    let rec halfs n =
+      if n = 0L then Seq.empty else seq { yield n; yield! halfs (n / 2L) }
+
+    if n = 0L then Seq.empty
+    else
+      let ns = halfs (n / 2L) |> Seq.map (fun x -> n - x)
+      seq { yield 0L; yield! interleave ns (ns |> Seq.map (fun x -> -1L * x)) }
+    |> Seq.map g
+
+  let shrinkByte = apply (shrinkUInt' uint64 byte)
+  let shrinkUInt16 = apply (shrinkUInt' uint64 uint16)
+  let shrinkUInt32 = apply (shrinkUInt' uint64 uint32)
+  let shrinkUInt64 = apply (shrinkUInt' uint64 id)
+  let shrinkSbyte = apply (shrinkInt' int64 sbyte)
+  let shrinkInt16 = apply (shrinkInt' int64 int16)
+  let shrinkInt = apply (shrinkInt' int64 int)
+  let shrinkInt64 = apply (shrinkInt' int64 id)
+
+  let shrinkString = apply (fun (s: string) ->
+    shrink (shrinkArray shrinkAny) (s.ToCharArray())
+    |> Seq.map(fun a -> System.String(a))
+  )
+
+  let shrinkOption s = apply (function
+    | None -> Seq.empty
+    | Some x -> seq {
+        yield None
+        for y in shrink s x -> Some(y)
+      })
+
+  let shrinkTuple2 s1 s2 = apply (fun (t1, t2) ->
+      let xs = shrink s1 t1 |> Seq.map (fun x -> (x, t2))
+      let ys = shrink s2 t2 |> Seq.map (fun x -> (t1, x))
+      Seq.append xs ys)
+
+  let shrinkTuple3 s1 s2 s3 = apply (fun (t1, t2, t3) ->
+      let xs = shrink s1 t1 |> Seq.map (fun x -> (x, t2, t3))
+      let ys = shrink s2 t2 |> Seq.map (fun x -> (t1, x, t3))
+      let zs = shrink s3 t3 |> Seq.map (fun x -> (t1, t2, x))
+      Seq.append (Seq.append xs ys) zs)
+
+  let xmap st (from: _ -> _) (to': _ -> _) = apply (fun u -> shrink (to' u) st |> Seq.map from)
