d79a219851b9bdb44ee8bb5d45ca2235aecfaa61
pocketberserker <pocketberserker@gmail.com>
2015-01-03 19:39:13 +0900

    add simple shrink test

---
+++ tests/Persimmon.Dried.Tests/ShrinkTest.fs
@@ -0,0 +1,13 @@
+﻿namespace Persimmon.Dried.Tests
+
+open Persimmon
+open Persimmon.Dried
+
+module ShrinkTest =
+
+  let ``int`` = property "int" {
+    prop (Prop.forAll Arb.int (fun n ->
+      Shrink.shrink Arb.int.Shrinker n
+      |> Seq.forall ((<>) n)
+      |> Prop.applyBool))
+  }
