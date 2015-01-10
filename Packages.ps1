00b26ad6526f47736cb3495b6740f1d5b8d42b83
pocketberserker <pocketberserker@gmail.com>
2015-01-10 14:42:08 +0900

    fix create package script

--- Packages.ps1
+++ Packages.ps1
@@ -1,4 +1,4 @@
-C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Persimmon.sln /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:rebuild
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild.exe Persimmon.Dried.sln /property:Configuration=Release /property:VisualStudioVersion=12.0 /target:rebuild
 
 .\.nuget\nuget.exe pack .\src\Persimmon.Dried\Persimmon.Dried.fsproj -Symbols -Properties VisualStudioVersion=12.0
 
