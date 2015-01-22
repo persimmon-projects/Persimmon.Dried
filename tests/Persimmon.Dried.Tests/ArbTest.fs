ede1cc1a5f6bedb20011b6eca2e07eebcac10181
pocketberserker <pocketberserker@gmail.com>
2015-01-23 02:17:19 +0900

    add Arb.func

---
+++ tests/Persimmon.Dried.Tests/ArbTest.fs
@@ -0,0 +1,11 @@
+﻿namespace Persimmon.Dried.Tests
+
+open Persimmon.Dried
+open UseTestNameByReflection
+
+module ArbTest =
+
+  let ``func`` = property {
+    apply (Prop.forAll (Arb.func CoArbitrary.int Arb.int, Arb.nonEmptyList Arb.int) (fun f xs ->
+      List.map f xs |> List.isEmpty |> not))
+  }
