d61c7d96b91e30559817c64d081ce2c9d204102a
pocketberserker <pocketberserker@gmail.com>
2015-01-23 19:41:12 +0900

    more test

--- tests/Persimmon.Dried.Tests/ArbTest.fs
+++ tests/Persimmon.Dried.Tests/ArbTest.fs
@@ -9,3 +9,10 @@ module ArbTest =
     apply (Prop.forAll (Arb.func CoArbitrary.int Arb.int, Arb.nonEmptyList Arb.int) (fun f xs ->
       List.map f xs |> List.isEmpty |> not))
   }
+
+  let ``func and fail`` =
+    let a = { Arb.nonEmptyList Arb.int with Gen = Gen.fail }
+    property {
+      apply (Prop.forAll (Arb.func CoArbitrary.int Arb.int, a) (fun f xs ->
+        List.map f xs |> List.isEmpty |> not) == Prop.undecided.Value)
+    }
