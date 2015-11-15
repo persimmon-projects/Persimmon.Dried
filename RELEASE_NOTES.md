4ab94b25992a5da427cb86779617fa215fb9a8db
pocketberserker <pocketberserker@gmail.com>
2015-11-15 12:17:10 +0900

    1.1.0

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,2 +1,7 @@
+### 1.1.0 - November 15 2015
+* add arbitrary computation expression
+* fix generate set
+* contain null value and support non null arbitrary
+
 ### 1.0.0 - October 18 2015
 * initial release
--- src/Persimmon.Dried.Ext/AssemblyInfo.fs
+++ src/Persimmon.Dried.Ext/AssemblyInfo.fs
@@ -7,8 +7,8 @@ open System.Runtime.InteropServices
 [<assembly: AssemblyProduct("Persimmon.Dried.Ext")>]
 [<assembly: Guid("6C0B494C-BBCC-4B99-9B75-C18C332AF359")>]
 
-[<assembly: AssemblyVersion("1.0.0")>]
-[<assembly: AssemblyFileVersion("1.0.0")>]
-[<assembly: AssemblyInformationalVersion("1.0.0")>]
+[<assembly: AssemblyVersion("1.1.0")>]
+[<assembly: AssemblyFileVersion("1.1.0")>]
+[<assembly: AssemblyInformationalVersion("1.1.0")>]
 
 do ()
