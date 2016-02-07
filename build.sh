ef11dc7155f505fcafcdfa89d1bc3f58921723e6
pocketberserker <pocketberserker@gmail.com>
2016-02-07 23:41:15 +0900

    fix fileformat

--- build.sh
+++ build.sh
@@ -14,10 +14,10 @@ then
   if [ $exit_code -ne 0 ]; then
   	exit $exit_code
   fi
-  
+
   [ ! -e build.fsx ] && .paket/paket.exe update
   [ ! -e build.fsx ] && packages/FAKE/tools/FAKE.exe init.fsx
-  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 else
   # use mono
   mono .paket/paket.bootstrapper.exe
@@ -34,5 +34,5 @@ else
 
   [ ! -e build.fsx ] && mono .paket/paket.exe update
   [ ! -e build.fsx ] && mono packages/FAKE/tools/FAKE.exe init.fsx
-  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx 
+  mono packages/FAKE/tools/FAKE.exe $@ --fsiargs -d:MONO build.fsx
 fi
