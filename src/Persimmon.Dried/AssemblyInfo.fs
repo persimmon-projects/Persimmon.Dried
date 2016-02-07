1c1ec1eaea651f345fffe745e3c681e329aa736d
pocketberserker <pocketberserker@gmail.com>
2016-02-07 21:47:49 +0900

    add FAKE script

--- src/Persimmon.Dried/AssemblyInfo.fs
+++ src/Persimmon.Dried/AssemblyInfo.fs
@@ -1,17 +1,17 @@
-﻿namespace global
-
+﻿namespace System
 open System.Reflection
-open System.Runtime.InteropServices
 open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
 
-[<assembly: AssemblyTitle("Persimmon.Dried")>]
-[<assembly: AssemblyProduct("Persimmon.Dried")>]
-[<assembly: Guid("735BAAC1-153E-44A1-B1CE-63516BFB732F")>]
-
-[<assembly: AssemblyVersion("1.2.0")>]
-[<assembly: AssemblyFileVersion("1.2.0")>]
-[<assembly: AssemblyInformationalVersion("1.2.0")>]
-
-[<assembly: InternalsVisibleTo("Persimmon.Dried.Tests")>]
-
+[<assembly: AssemblyTitleAttribute("Persimmon.Dried")>]
+[<assembly: AssemblyDescriptionAttribute("")>]
+[<assembly: InternalsVisibleToAttribute("Persimmon.Dried.Tests")>]
+[<assembly: GuidAttribute("735BAAC1-153E-44A1-B1CE-63516BFB732F")>]
+[<assembly: AssemblyProductAttribute("Persimmon.Dried")>]
+[<assembly: AssemblyVersionAttribute("1.2.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.2.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.2.0")>]
 do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.2.0"
